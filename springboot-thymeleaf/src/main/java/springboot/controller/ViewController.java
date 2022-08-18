795e40df81ac876c227453a4aad255b5f87075fb
chenyuhui <2236212007@qq.com>
2022-08-19 03:12:20 +0800

    springboot关于thymeleaf模板的使用

---
+++ springboot-thymeleaf/src/main/java/springboot/controller/ViewController.java
@@ -0,0 +1,23 @@
+package springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        //model中的数据会被放在请求域中 request.setAttribute("a",aa)
+        model.addAttribute("msg","一定要大力发展工业文化");
+        model.addAttribute("link","http://www.baidu.com");
+        return "success";
+    }
+
+
+
+
+
+}
