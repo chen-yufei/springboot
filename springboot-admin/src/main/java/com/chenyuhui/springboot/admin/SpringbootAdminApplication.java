5a3404849fe9ef3ff939786dc25c4a10979b4383
chenyuhui <2236212007@qq.com>
2022-08-20 22:55:46 +0800

    springboot项目-原生组件注入-原生注解与Spring方式注入

--- springboot-admin/src/main/java/com/chenyuhui/springboot/admin/SpringbootAdminApplication.java
+++ springboot-admin/src/main/java/com/chenyuhui/springboot/admin/SpringbootAdminApplication.java
@@ -2,8 +2,10 @@ package com.chenyuhui.springboot.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.chenyuhui.springboot.admin.servlet")
 public class SpringbootAdminApplication {
 
     public static void main(String[] args) {
