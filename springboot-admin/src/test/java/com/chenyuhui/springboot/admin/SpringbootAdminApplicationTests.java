0c0ebc3cd27e35c79eba3a5521d028e5f7f82f4a
chenyuhui <2236212007@qq.com>
2022-08-21 01:22:22 +0800

    springboot项目-切换web服务器&导入JDBC场景

--- springboot-admin/src/test/java/com/chenyuhui/springboot/admin/SpringbootAdminApplicationTests.java
+++ springboot-admin/src/test/java/com/chenyuhui/springboot/admin/SpringbootAdminApplicationTests.java
@@ -1,13 +1,27 @@
 package com.chenyuhui.springboot.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class SpringbootAdminApplicationTests {
 
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from t_cyh_user",Long.class);
+        log.info("记录数为:{}",count);
     }
+
+
+
 
 }
